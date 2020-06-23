789ed65c8e9dc2148811ca3c474f540d3d00d841
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-06-23 15:55:27 -0700

    end of day commit, out of time
    Teresa Rosinski <trosinski89@gmail.com>

--- __test__/sudoku.test.js
+++ __test__/sudoku.test.js
@@ -1,22 +1,19 @@
-import { sudokuChecker } from "./../src/sudoku.js";
+import { sudokuChecker, sudokuRowAndColChecker } from "./../src/sudoku.js";
 
-const numbers = {
-  "1": 1,
-  "2": 1,
-  "3": 0,
-  "4": 2,
-  "5": 1,
-  "6": 1,
-  "7": 1,
-  "8": 1,
-  "9": 1
-};
+const row = { "1": 1, "2": 1, "3": 1, "4": 1, "5": 1, "6": 1, "7": 1, "8": 1, "9": 1 };
+const column = { "1": 1, "2": 1, "3": 1, "4": 1, "5": 1, "6": 1, "7": 1, "8": 1, "9": 1 };
+
 
 describe("sudokuChecker", () => {
   
-  test("Should take in an numObject with more than one or less than one number each and return false", () => 
+  test("Should take in an number Object with one of each number between 1 - 9 and return true", () => 
   {
-    expect(sudokuChecker(numbers)).toEqual(false);
+    expect(sudokuChecker(row)).toEqual(true);
+  });
+
+  test("Should take in two number Objects with one of each number between 1 - 9 and return true", () => 
+  {
+    expect(sudokuRowAndColChecker(row, column)).toEqual(true);
   });
 });
 
--- src/sudoku.js
+++ src/sudoku.js
@@ -2,7 +2,97 @@ export function sudokuChecker(numObject) {
   const values = Object.values(numObject);
   for (let i = 0; i < values.length; i++){
     if (values[i] !== 1)
-      return false; 
+      return false;
   }
   return true;
 }
+
+
+
+function SudokuRowAndColumn() {
+  this.rowAndColumn = [];
+}
+
+SudokuRowAndColumn.prototype.testRow = function (row) {
+  const rowValues = Object.values(row);
+  for (let i = 0; i < rowValues.length; i++) {
+    if (rowValues[i] !== 1) {
+      return false;
+    }
+    this.rowAndColumn.push(rowValues[i]);
+  }
+  return true;
+}
+
+SudokuRowAndColumn.prototype.testColumn = function (col) {
+  const colValues = Object.values(col);
+  for (let i = 0; i < colValues.length; i++){
+    if (colValues[i] !== 1) {
+      return false;
+    }
+    this.rowAndColumn[i] += colValues[i];
+  }
+  return true
+}
+
+SudokuBoard.prototype.combineRowAndColumn = function () {
+
+}
+
+SudokuBoard.prototype.testRowAndColumn = function () {
+  
+}
+
+
+
+
+
+// This is taking in the number object that has the key-value pairs for each row and columnn where the number is the key and the value is how many of those numbers exist
+export function sudokuRowAndColChecker(row, col) {
+  const rowValues = Object.values(row);
+  const colValues = Object.values(col);
+  let rowAndColValues = [];
+  for (let i = 0; i < rowValues.length; i++) {
+    if (rowValues[i] !== 1) {
+      return false;
+    }
+    rowAndColValues.push(rowValues[i]);
+  }
+
+  for (let i = 0; i < colValues.length; i++){
+    if (colValues[i] !== 1) {
+      return false;
+    }
+    rowAndColValues[i] += colValues[i];
+  }
+
+  for (let i = 0; i < rowAndColValues.length; i++){
+    if (rowAndColValues[i] !== 2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const numbers = {
+  "1": 1,
+  "2": 1,
+  "3": 0,
+  "4": 2,
+  "5": 1,
+  "6": 1,
+  "7": 1,
+  "8": 1,
+  "9": 1
+};
+
+
+
+
+
+// row
+// 223344551
+
+// column
+// 166778899
